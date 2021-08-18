122589b799f9dfdbcefdabc71d97b7ae767d685f
Kishore-kavan <71962525+Kishore-kavan@users.noreply.github.com>
2021-08-18 23:41:03 +0530

    Update my script.js

--- my script.js
+++ my script.js
@@ -1,1 +1,26 @@
+          var outputScreen = document.getElementById("output-screen");
 
+      function display(num){
+        outputScreen.value += num;
+      }
+      function calculate()
+      {
+        try{
+          outputScreen.value= eval(outputScreen.value);
+                  }
+                  catch(err)
+                  {
+                    alert("wrong calculation")
+                  }
+      }
+function Clear()
+{
+outputScreen.value="";
+}
+function Del()
+{
+outputScreen.value= outputScreen.value.slice(0,-1);
+}
+
+
+    
